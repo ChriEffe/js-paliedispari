3d5fa6e60d341c32822e29d5657a3a60238cc271
ChriEffe <christianferrazzano93@gmail.com>
2021-11-29 15:06:38 +0100

    win or lose

--- pali e dispari/js/script.js
+++ pali e dispari/js/script.js
@@ -5,3 +5,27 @@ Stabiliamo se la somma dei due numeri è pari o dispari
 Dichiariamo chi ha vinto
 (corrispondenza tra scelta pari e dispari e somma pari o dispari)
 In un secondo momento creiamo delle funzioni per i numeri random e per il check pari dispari*/
+
+
+let parioDispari = prompt('Pari o dispari?');
+let number = parseInt(prompt('Inserisci un numero da 1 a 5'));
+console.log(parioDispari);
+console.log('Numero utente ' + number);
+
+randomNumber = Math.floor(Math.random() * 6);
+console.log('Numero random ' + randomNumber);
+
+let sum = number += randomNumber
+console.log('Risultato ' + sum);
+
+if (sum % 2 != 0) {
+    console.log('Dispari');
+} else {
+    console.log('Pari');
+};
+
+if (parioDispari == sum) {
+    console.log('Hai vinto');
+} else {
+    console.log('Hai perso');
+};
